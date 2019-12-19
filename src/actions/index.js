f0e1e53c5419f097d352b906d52d388b9ab8b2cf
sunny <sunnysultan1640@gmail.com>
2019-12-19 15:54:11 +0600

    update import in action page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
-import getScore from '../api/getMainApi'
+import getPost from '../api/getMainApi'
 
 
 export const getAllPost = () =>async (dispatch) =>{
     try {
-        const response = await getScore.get('/posts');
+        const response = await getPost.get('/posts');
         dispatch({type : "ALL_POST",payload: response.data});
     } catch (error) {
         console.log(error);
